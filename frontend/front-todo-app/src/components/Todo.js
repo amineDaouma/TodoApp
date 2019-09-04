b2857f855903a0b124ce3dc0d8a3949c5809fa05
amine.daouma <amine.daouma@outscale.com>
2019-09-04 11:43:16 +0200

    introduce the long todo text in storybook

--- frontend/front-todo-app/src/components/Todo.js
+++ frontend/front-todo-app/src/components/Todo.js
@@ -6,16 +6,24 @@ const Todo = ({ todo, completeTodo, removeTodo }) => {
     return (
       <div
         className="todo"
-        style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+        style={{
+          textDecoration: todo.isCompleted ? "line-through" : "",
+          textOverflow: "ellipsis"
+        }}
       >
         {todo.text}
         <button
-          style={{ background: "green" }}
+          style={{ background: "green", display: "inline-block" }}
           onClick={() => completeTodo(todo.id, todo.isCompleted)}
         >
           Complete
         </button>
-        <button onClick={() => removeTodo(todo.id)}>Remove</button>
+        <button
+          style={{ background: "blue", display: "inline-block" }}
+          onClick={() => removeTodo(todo.id)}
+        >
+          Remove
+        </button>
       </div>
     );
   } else {
--- frontend/front-todo-app/src/components/Todo.stories.js
+++ frontend/front-todo-app/src/components/Todo.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs/react";
 
 import Todo from "./Todo";
 
@@ -15,10 +16,20 @@ export const actions = {
   completeTodo: action("completeTodo"),
   removeTodo: action("removeTodo")
 };
+const longTodoText = `This tasks name is absurdly large. 
+In fact, I think if I keep going I might end up with content overflow. 
+What will happen? The star that represents a pinned task could have text overlapping.
+The text could cut-off abruptly when it reaches the star. I hope not. 
+Well, You have to give it another try to see if everything could fit together in the CoreUX app.
+I just wanna write anything to fill the space left to test the long title use case `;
 
 storiesOf("Todo", module)
-  .add("default", () => <Todo todo={todo} {...actions} />)
+  .addDecorator(withKnobs)
+  .add("default", () => (
+    <Todo todo={object("todo", { ...todo })} {...actions} />
+  ))
   .add("completed", () => (
     <Todo todo={{ ...todo, isCompleted: true }} {...actions} />
   ))
-  .add("deleted", () => <Todo todos={{}} />);
+  .add("deleted", () => <Todo todos={{}} />)
+  .add("longTodoText", () => <Todo todo={{ ...todo, text: longTodoText }} />);
